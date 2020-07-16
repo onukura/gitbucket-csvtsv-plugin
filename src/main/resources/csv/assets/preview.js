9abb30c5eb6b0ede9e4390f7cb364b6d48f5ddd6
onukura <y.bibos.ishikawa@gmail.com>
2020-07-16 23:57:32 +0900

    change preview width

--- src/main/resources/csv/assets/preview.js
+++ src/main/resources/csv/assets/preview.js
@@ -1,6 +1,6 @@
 function fixWidth () {
     var element = document.getElementsByClassName("content body clearfix")
     var prv = document.getElementById("preview")
-    prv.style.width = String(element[0].offsetWidth*0.95) + "px"
+    prv.style.width = String(element[0].offsetWidth - 40) + "px"
 }
 window.onload = fixWidth();
